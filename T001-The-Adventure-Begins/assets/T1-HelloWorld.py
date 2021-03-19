b8cba70a31878f15a3d9c1c9fbb42ed8e3dd0ba6
kghite <kghite@gmail.com>
2021-03-19 00:00:51 -0400

    T1 exercises + src fixes

---
+++ T001-The-Adventure-Begins/assets/T1-HelloWorld.py
@@ -0,0 +1,5 @@
+# This is the code editor!
+
+# Try using the Console by typing print("Hello World!") in the Replit Console tab and then pressing Enter on your keyboard, just like we saw in the previous video.
+
+# What happens if you replace the words Hello World! inside the quotation marks?  Try printing out your own Python message!
