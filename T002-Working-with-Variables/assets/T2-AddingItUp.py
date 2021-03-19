633e51630b01e8b0ebecb0a3ebf6b9b05bbf9fa7
kghite <kghite@gmail.com>
2021-03-19 00:00:51 -0400

    T1 exercises + src fixes

---
+++ T002-Working-with-Variables/assets/T2-AddingItUp.py
@@ -0,0 +1,9 @@
+# Adding It Up
+
+# Step 1: Initialize the variable num_fish with the integer value 8.
+
+# Step 2: Intialize the variable num_hamsters with the integer value 2.
+
+# Step 3: Enter the line num_fish + num_hamsters into the console. How many pets are there total? How can you assign the results of that data operation to a variable named total_pets?
+
+# Step 4: Initialize two of your own variables! Then use your two variables to try out each of the four arithmentic operators that we talked about in the last video.
---
+++ T002-Working-with-Variables/assets/T2-CreatingVariables.py
@@ -0,0 +1,11 @@
+# Creating Variables
+
+# Step 1: Type the line age = 22 into the console and press Enter on your keyboard.
+
+# Step 2: Now enter the line age. What does the console output in response? Why do you think it gave you that value?
+
+# Step 3: Let's make another variable in the console called your_age and assign it the value of how old you are. How can you check the value of the variable your_age after you initialize it?
+
+# Step 4: Now try reassigning the value of age to your_age using the line age = your_age in the console. When you type age again and press Enter, what do you see?  Why?
+
+# Step 5: Choose your own variable to initialize! Try reassigning it to a new value.
