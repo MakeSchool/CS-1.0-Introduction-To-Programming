e3939f67a023693a8b6af97565ef47958489a4be
kghite <kghite@gmail.com>
2021-04-12 15:16:25 -0400

    RTS - projects

---
+++ T011-Build-a-Python-Poet/assets/T11-PythonPoetBuild.py
@@ -0,0 +1,13 @@
+# Python Poet Build
+
+# Step 1: Store your poem in a text file
+
+# Step 2: Create a function called get_file_lines that takes the parameter filename and returns a list of the strings contained in the file
+
+# Step 3: Create a function called lines_printed_backward that takes a parameter called lines_list, which is a list containing the lines of your poem.  It should print the lines of the poem in reverse and include the original line number at the beginning of each line
+
+# Step 4: Create a function called lines_printed_random that takes a parameter called lines_list, which is a list containing the lines of your poem. It should print the lines of your poem in random order
+
+# Step 5: Create a function called lines_printed_custom that takes at least one parameter called lines_list, which is a list containing the lines of your poem. It should print the poem in a unique way of your choice
+
+# Step 6: Your final code should use all four functions to print out your poem backwards, randomly, and in your own custom way
