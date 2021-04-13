13faefc1230e99cbead34de4e828ea9ea42216e5
kghite <kghite@gmail.com>
2021-04-13 03:23:44 -0400

    RTS - All exercises

---
+++ T007-Loops/assets/T7-MakingWhileLoops.py
@@ -0,0 +1,6 @@
+# Making While Loops
+
+# Step 1: It's sale season, and the prices of socks are dropping every day by $0.70 at a large department store. Use a while loop to print the value of the variable price. It should decrease by $0.70 each iteration, as long as it remains positive.
+price = 5.90
+
+# Step 2: Use a while loop to print the even numbers between 0 and 20
---
+++ T010-Dictionaries-and-Code-Quality/assets/T10-AirTrafficControl.py
@@ -0,0 +1,5 @@
+# Air Traffic Control
+
+# Step 1: Create a dictionary called flights with at least 3 keys. Each key should be an integer flight number and map to a string value representing the destination for the flight.
+
+# Step 2: Print your dictionary of flights
