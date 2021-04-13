bb0b14e610625fd2c97e0022898251b997eb2a96
kghite <kghite@gmail.com>
2021-04-12 22:50:03 -0400

    RTS - T6

---
+++ T006-Lists-and-Pseudocode/assets/T6-StockPrices.py
@@ -0,0 +1,9 @@
+# Stock Prices
+
+# Step 1: Create a list called stock_history with the Open, High, and Low stock prices for APPL as float values. Then print the list to the Python console check the values.
+
+# Step 2: Use list indexing to access and print the High value of the stock for the day. Remember that the first value in the list is accessed with the index 0!
+
+# Step 3: Use negative list indexing to print the Low value of the stock for the day (the last element in the list).
+
+# Step 4: Using list slicing, print out both the High and Low stock prices at the same time.
